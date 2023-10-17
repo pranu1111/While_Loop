3661af712c67845de27253f6bc9423b9a1d33358
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-10-17 10:43:00 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,43 @@
+#i = 1
+#while i < 6:
+#    print(i)
+#    i += 1
+#
+#i = 10
+#while i < 15:
+#    print("Pranita")
+#    i += 1
+#
+#i = 0
+#while i < 50:
+#    print(i)
+#    i += 1
+
+#i = 1
+#while i < 6:
+#    print(i)
+#    if i == 3:
+#        break
+#    i += 1
+
+
+#i = 5
+#while i < 10:
+#    print("Pranita")
+#    if i == 8:
+#        break
+#    i += 1
+    
+#i = 0
+#while i < 10:
+#    i += 1
+#    if i == 3 or i == 6:
+#        continue
+#    print(i)
+    
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")    
